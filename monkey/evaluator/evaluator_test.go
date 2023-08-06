50e654d3dd9e51ebb0f836a655828d97b67bbbee
Andrew Robbins <andydude@users.noreply.github.com>
2023-08-05 23:32:39 -0400

    Added semicolons to evaluator test

--- monkey/evaluator/evaluator_test.go
+++ monkey/evaluator/evaluator_test.go
@@ -11,21 +11,21 @@ func TestEvalIntegerExpression(t *testing.T) {
 		input    string
 		expected int64
 	}{
-		{"5", 5},
-		{"10", 10},
-		{"-5", -5},
-		{"-10", -10},
-		{"5 + 5 + 5 + 5 - 10", 10},
-		{"2 * 2 * 2 * 2 * 2", 32},
-		{"-50 + 100 + -50", 0},
-		{"5 * 2 + 10", 20},
-		{"5 + 2 * 10", 25},
-		{"20 + 2 * -10", 0},
-		{"50 / 2 * 2 + 10", 60},
-		{"2 * (5 + 10)", 30},
-		{"3 * 3 * 3 + 10", 37},
-		{"3 * (3 * 3) + 10", 37},
-		{"(5 + 10 * 2 + 15 / 3) * 2 + -10", 50},
+		{"5;", 5},
+		{"10;", 10},
+		{"-5;", -5},
+		{"-10;", -10},
+		{"5 + 5 + 5 + 5 - 10;", 10},
+		{"2 * 2 * 2 * 2 * 2;", 32},
+		{"-50 + 100 + -50;", 0},
+		{"5 * 2 + 10;", 20},
+		{"5 + 2 * 10;", 25},
+		{"20 + 2 * -10;", 0},
+		{"50 / 2 * 2 + 10;", 60},
+		{"2 * (5 + 10);", 30},
+		{"3 * 3 * 3 + 10;", 37},
+		{"3 * (3 * 3) + 10;", 37},
+		{"(5 + 10 * 2 + 15 / 3) * 2 + -10;", 50},
 	}
 
 	for _, tt := range tests {
@@ -39,25 +39,25 @@ func TestEvalBooleanExpression(t *testing.T) {
 		input    string
 		expected bool
 	}{
-		{"true", true},
-		{"false", false},
-		{"1 < 2", true},
-		{"1 > 2", false},
-		{"1 < 1", false},
-		{"1 > 1", false},
-		{"1 == 1", true},
-		{"1 != 1", false},
-		{"1 == 2", false},
-		{"1 != 2", true},
-		{"true == true", true},
-		{"false == false", true},
-		{"true == false", false},
-		{"true != false", true},
-		{"false != true", true},
-		{"(1 < 2) == true", true},
-		{"(1 < 2) == false", false},
-		{"(1 > 2) == true", false},
-		{"(1 > 2) == false", true},
+		{"true;", true},
+		{"false;", false},
+		{"1 < 2;", true},
+		{"1 > 2;", false},
+		{"1 < 1;", false},
+		{"1 > 1;", false},
+		{"1 == 1;", true},
+		{"1 != 1;", false},
+		{"1 == 2;", false},
+		{"1 != 2;", true},
+		{"true == true;", true},
+		{"false == false;", true},
+		{"true == false;", false},
+		{"true != false;", true},
+		{"false != true;", true},
+		{"(1 < 2) == true;", true},
+		{"(1 < 2) == false;", false},
+		{"(1 > 2) == true;", false},
+		{"(1 > 2) == false;", true},
 	}
 
 	for _, tt := range tests {
@@ -71,12 +71,12 @@ func TestBangOperator(t *testing.T) {
 		input    string
 		expected bool
 	}{
-		{"!true", false},
-		{"!false", true},
-		{"!5", false},
-		{"!!true", true},
-		{"!!false", false},
-		{"!!5", true},
+		{"!true;", false},
+		{"!false;", true},
+		{"!5;", false},
+		{"!!true;", true},
+		{"!!false;", false},
+		{"!!5;", true},
 	}
 
 	for _, tt := range tests {
@@ -90,13 +90,13 @@ func TestIfElseExpressions(t *testing.T) {
 		input    string
 		expected interface{}
 	}{
-		{"if (true) { 10 }", 10},
-		{"if (false) { 10 }", nil},
-		{"if (1) { 10 }", 10},
-		{"if (1 < 2) { 10 }", 10},
-		{"if (1 > 2) { 10 }", nil},
-		{"if (1 > 2) { 10 } else { 20 }", 20},
-		{"if (1 < 2) { 10 } else { 20 }", 10},
+		{"if (true) { 10 };", 10},
+		{"if (false) { 10 };", nil},
+		{"if (1) { 10 };", 10},
+		{"if (1 < 2) { 10 };", 10},
+		{"if (1 > 2) { 10 };", nil},
+		{"if (1 > 2) { 10 } else { 20 };", 20},
+		{"if (1 < 2) { 10 } else { 20 };", 10},
 	}
 
 	for _, tt := range tests {
@@ -318,7 +318,7 @@ ourFunction(20) + first + second;`
 func TestClosures(t *testing.T) {
 	input := `
 let newAdder = fn(x) {
-  fn(y) { x + y };
+  fn(y) { x + y; };
 };
 
 let addTwo = newAdder(2);
