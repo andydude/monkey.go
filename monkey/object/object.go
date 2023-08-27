b44fd9c44c1e5a33485f413ac604dd3f77be4473
Andrew Robbins <andydude@users.noreply.github.com>
2023-08-26 20:10:12 -0400

    Fork WACIG

--- monkey/object/object.go
+++ monkey/object/object.go
@@ -1,5 +1,3 @@
-// SPDX-FileCopyrightText: 2016-2017 Thorsten Ball
-// SPDX-License-Identifier: MIT
 package object
 
 import (
@@ -7,6 +5,7 @@ import (
 	"fmt"
 	"hash/fnv"
 	"monkey/ast"
+	"monkey/code"
 	"strings"
 )
 
@@ -29,6 +28,8 @@ const (
 
 	ARRAY_OBJ = "ARRAY"
 	HASH_OBJ  = "HASH"
+
+	COMPILED_FUNCTION_OBJ = "COMPILED_FUNCTION_OBJ"
 )
 
 type HashKey struct {
@@ -181,4 +182,15 @@ func (h *Hash) Inspect() string {
 	out.WriteString("}")
 
 	return out.String()
+}
+
+type CompiledFunction struct {
+	Instructions  code.Instructions
+	NumLocals     int
+	NumParameters int
+}
+
+func (cf *CompiledFunction) Type() ObjectType { return COMPILED_FUNCTION_OBJ }
+func (cf *CompiledFunction) Inspect() string {
+	return fmt.Sprintf("CompiledFunction[%p]", cf)
 }
